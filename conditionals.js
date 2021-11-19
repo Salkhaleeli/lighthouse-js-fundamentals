058160ce33ef848a73ffe116b8dc00259296d1a8
Saad Alkhaleeli <saad@PhilsMacBookPro.hitronhub.home>
2021-11-19 16:01:18 -0700

    isEven and isOdd functions

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,73 @@ if (temperature < 0) {
 }
 
 console.log("temperature today is", (temperature));
+
+let whichSchool = function (age) {
+    if (age < 13) {
+        return "Elementary School";
+    } else if (13 <= age && age <= 18) {
+        return "Secondary School";    
+    } else if (age > 18) {
+        return "Lighthouse Labs";
+    }
+}
+whichSchool(12);
+
+
+
+let x = 1;
+
+while (x <= 20) {
+    if (x % 3 === 0) {
+        console.log('Julia');
+        
+    } else if (x % 5 === 0) {
+        console.log('James');        
+    } else if (x % 3 === 0 && x % 5 === 0) {
+        console.log('JuliaJames');
+        
+    } else {
+        console.log(x);
+    }
+    x = x +1;
+    
+}
+
+let x = 9
+for (x = 0; x >= 1; x--) {
+    console.log('hello ' +x);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ isEvenisOdd.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+console.log(isEven(10));
+console.log(isEven(13));
+
+
+const isOdd = function(num) {
+  return num % 2 === 1;
+}
+console.log(isOdd(3));
+console.log(isOdd(8));
