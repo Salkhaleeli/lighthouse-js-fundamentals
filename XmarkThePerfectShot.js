0b7e2359c96238ed7cdf39709734061ee9c12eb8
Saad Alkhaleeli <saad@PhilsMacBookPro.hitronhub.home>
2021-11-20 16:47:05 -0700

    X Marks The Perfect Shot

---
+++ XmarkThePerfectShot.js
@@ -0,0 +1,23 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+
+const finalPosition = function (moves) {
+
+  for (const move of moves) {
+   let position = [0, 0];   
+    if (move === 'north') {
+      position[1] = position[1] + 1 ;
+    }
+    else if (move === 'south') {
+      position[1] = position[1] - 1;
+    }
+    else if (move === 'east') {
+      position[0] = position[0] + 1;
+    }
+    else if (move === 'west') {
+      position[0] = position[0] - 1;
+    }
+  }
+  return position;
+  
+};
+console.log(finalPosition(moves));
