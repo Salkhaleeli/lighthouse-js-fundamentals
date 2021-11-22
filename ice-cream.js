2cdd2e44d1d88b06736775789a9535edd8a05b5b
Saad Alkhaleeli <saad@PhilsMacBookPro.hitronhub.home>
2021-11-21 22:29:39 -0700

    Ice cream flav array

---
+++ ice-cream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+const firstFlav = iceCreamFlavours[0];
+console.log('Ban\'s favorite flavor is ' + firstFlav);
+const lastFlav = iceCreamFlavours[5];
+console.log('Atheer\'s favorite flavor is ' + lastFlav);
+console.log('Total no of flavors is ' + iceCreamFlavours.length);
