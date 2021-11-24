8aa5d9f286aa2a6ab588de6157356515a3241ffb
Saad Alkhaleeli <saad@PhilsMacBookPro.hitronhub.home>
2021-11-24 12:30:28 -0700

    concat arrays

---
+++ concat.js
@@ -0,0 +1,8 @@
+const concat = function(array1, array2) {
+    let output = [array1.concat(array2)];
+    return output;
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
